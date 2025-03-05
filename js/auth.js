1652a09308a4af8c8b601f01531a1733e9a1740e
Sameer Mahindru <113050438+CodesBySammy@users.noreply.github.com>
2025-03-05 21:57:10 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -22,7 +22,7 @@ function login() {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./public/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then(reg => console.log("Service Worker Registered", reg))
             .catch(err => console.log("Service Worker Registration Failed", err));
     });
